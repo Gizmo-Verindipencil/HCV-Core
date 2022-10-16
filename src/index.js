7c457f72d49eb53d89b75cde4783fef4bdfcf3c2
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-10-16 22:32:50 +0900

    Publish an additional object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { Color } from "./color";
 import { ColorCreator } from "./creator/color-creator";
 import { ColorDetector } from "./detector/color-detector";
 import { ColorExtractor } from "./extractor/color-extractor";
 
+exports.color = Color;
 exports.creator = new ColorCreator();
 exports.detector = new ColorDetector();
 exports.extractor = new ColorExtractor();
