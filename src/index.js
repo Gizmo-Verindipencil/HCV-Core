b343c0feb8c034fee7f3c9808417ae8e41ee07bd
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-10-20 22:00:55 +0900

    Publish color expression types

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { Color } from "./color";
+import { ColorExpressionType } from "./color-expression-type";
 import { ColorCreator } from "./creator/color-creator";
 import { ColorDetector } from "./detector/color-detector";
 import { ColorExtractor } from "./extractor/color-extractor";
 
 exports.color = Color;
+exports.type = ColorExpressionType;
 exports.creator = new ColorCreator();
 exports.detector = new ColorDetector();
 exports.extractor = new ColorExtractor();
